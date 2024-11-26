9263813ee53604534d8793d0179c928c045394b5
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-26 09:45:02 -0800

    Update Expanded-form.js

--- Expanded-form.js
+++ Expanded-form.js
@@ -22,3 +22,12 @@ const expandedForm = n => n.toString()
                             .join(" + ");
 
 // or
+
+function expandedForm(num) {
+  let arr = num.toString().split('').reverse();
+  let result = [];
+  for(let i = 0; i < arr.length; i++){
+    arr[i] == 0 ? result.push() : result.push(arr[i] + ('0'.repeat(i)))
+  }
+  return result.reverse().join(' + ')
+}
